62724cc75d8653e89ccc423f5975df4e91bf84a5
Kristján Oddsson <koddsson@gmail.com>
2016-03-28 14:42:13 +0000

    Add 'mark as done' and 'add item' features

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const fetch = require('node-fetch');
+const fs = require('fs');
 
-const USERNAME = 'koddsson';
+const configData = fs.readFileSync('.github-auth.json', 'utf8');
+const config = JSON.parse(configData);
 
+// XXX: Get some nice CLI utils to deal with all this.
+const command = process.argv[2];
+const args = process.argv.slice(3);
+
+// XXX: Move into utils?
 const removeTodoMarkdown = (line) => {
   if (line[0] === ']') {
     return line.substr(1).trim();
@@ -9,28 +16,72 @@ const removeTodoMarkdown = (line) => {
   return removeTodoMarkdown(line.substr(1));
 };
 
-fetch(
-  `https://api.github.com/repos/${USERNAME}/todo/contents/todo.md\?ref\=master`,
-  {
+const apiUrl = (
+  `https://api.github.com/repos/${config.username}/todo/contents/todo.md`
+);
+const userPass = (
+  `${config.username}:${config.password}`
+);
+
+const getItems = () => (
+  fetch(apiUrl).then(res => res.json()).then(json => {
+    return {
+      items: new Buffer(json.content, 'base64').toString('ascii').split('\n'),
+      sha: json.sha,
+    };
+  })
+);
+
+const setItems = (contents, sha) => {
+  fetch(apiUrl, {
+    method: 'PUT',
     headers: {
-      Accept: 'application/vnd.github.VERSION.raw',
+      Authorization: `Basic ${new Buffer(userPass).toString('base64')}`,
     },
-  }
-).then(res => res.text())
- .then(text => {
-   const lines = text.split('\n');
-   const doneItems = lines.filter(line => {
-     return line.includes('- [x]') || line.includes('-[x]');
-   });
-   const notDoneItems = lines.filter(line => {
-     return line.includes('- [ ]') || line.includes('-[ ]');
-   });
+    body: JSON.stringify({
+      message: 'Add a new TODO item',
+      content: new Buffer(contents.filter(x => x).join('\n')).toString('base64'),
+      sha: sha,
+    }),
+  }).then(res => {
+    return res.text();
+  });
+};
 
-   const formattedLines = notDoneItems.map(removeTodoMarkdown);
+if (command === 'mark') {
+  getItems().then(json => {
+    const doneItems = json.items.filter(item => {
+      return item.includes('- [x]') || item.includes('-[x]');
+    });
+    const notDoneItems = json.items.filter(item => {
+      return item.includes('- [ ]') || item.includes('-[ ]');
+    });
+    const newItems = notDoneItems.map((item, index) => {
+      if (Number(args[0]) === index + 1) {
+        return `- [x] ${removeTodoMarkdown(item)}`;
+      }
+      return item;
+    });
+    setItems(newItems.concat(doneItems), json.sha);
+  });
+} else if (command === 'add') {
+  getItems().then(json => {
+    const items = json.items;
+    items.push(`- [ ] ${args[0]}`);
+    setItems(items, json.sha).then(text => console.log(text));
+  });
+} else {
+  getItems().then(json => {
+    const items = json.items;
+    const notDoneItems = items.filter(item => {
+      return item.includes('- [ ]') || item.includes('-[ ]');
+    });
+    const formattedLines = notDoneItems.map(removeTodoMarkdown);
 
-   if (formattedLines.length) {
-     formattedLines.forEach(line => console.log(`- ${line}`));
-   } else {
-     console.log('Nothing on your todo list! Good job! 🎉');
-   }
-});
+    if (formattedLines.length) {
+      formattedLines.forEach((item, index) => console.log(`${index + 1}. ${item}`));
+    } else {
+      console.log('Nothing on your todo list! Good job! 🎉');
+    }
+  });
+}
